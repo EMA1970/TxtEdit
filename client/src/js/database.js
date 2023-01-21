f74f476e3057ba877186bfc3cf5e36295986e5f9
EMA1970 <114694158+EMA1970@users.noreply.github.com>
2023-01-20 22:52:46 -0800

    fixed bug related to workpack import statement

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -17,8 +17,7 @@ export const putDb = async (content) => {
   try { 
     console.log('PUT method');
     // creates a connection to the db and version 
-    const jateDb = await openDB('jate',1)
-    console.log('Content added to jate db');
+    const jateDb = await openDB('jate',1);
     // creates a new transaction and specify database and data privilages 
     const tx = jateDb.transaction('jate', 'readwrite');
     // open up the desired object store 
@@ -27,14 +26,11 @@ export const putDb = async (content) => {
     const request = store.add({ id: 1, value: content });
     // get confirmation of the request 
     const result = await request;
-  console.log('🚀 - data saved to the database', result);
+    console.log('🚀 - data saved to the database', result);
   }catch (err) {
     console.error('PUT not implemented');
   }
 };
-
-
-console.error('putDb not implemented');
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
@@ -63,26 +59,26 @@ export const getDb = async () => {
 
 
 
-export const getOneDb = async (id) => {
-  console.log('GET from the database');
+// export const getOneDb = async (id) => {
+//   console.log('GET from the database');
 
-  // Create a connection to the database database and version we want to use.
-  const jateDb = await openDB('jate', 1);
+//   // Create a connection to the database database and version we want to use.
+//   const jateDb = await openDB('jate', 1);
 
-   // Create a new transaction and specify the database and data privileges.
-  const tx = jateDb.transaction('jate', 'readonly');
+//    // Create a new transaction and specify the database and data privileges.
+//   const tx = jateDb.transaction('jate', 'readonly');
 
-  // Open up the desired object store.
-  const store = tx.objectStore('jate');
+//   // Open up the desired object store.
+//   const store = tx.objectStore('jate');
 
-   // Use the .get() method to get a piece of data from the database based on the id.
-  const request = store.get(id);
+//    // Use the .get() method to get a piece of data from the database based on the id.
+//   const request = store.get(id);
 
-  // Get confirmation of the request.
-  const result = await request;
-  console.log('result.value', result);
-  return result;
-};
+//   // Get confirmation of the request.
+//   const result = await request;
+//   console.log('result.value', result);
+//   return result;
+// };
 
 initdb();
 
